c3efff7b2d41fb0156adc6522d45c283266a7151
chroniclesofcode <112388695+chroniclesofcode@users.noreply.github.com>
2022-09-06 21:04:24 +1000

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -8,25 +8,40 @@ project "GLFW"
     files {
         "include/GLFW/glfw3.h",
         "include/GLFW/glfw3native.h",
-        "src/glfw_config.h",
+        "src/internal.h",
+        "src/platform.h",
+        "src/mappings.h",
         "src/context.c",
         "src/init.c",
         "src/input.c",
         "src/monitor.c",
+        "src/platform.c",
         "src/vulkan.c",
-        "src/window.c"
+        "src/window.c",
+        "src/egl_context.c",
+        "src/osmesa_context.c",
+        "src/null_platform.h",
+        "src/null_joystick.h",
+        "src/null_init.c",
+
+        "src/null_monitor.c",
+        "src/null_window.c",
+        "src/null_joystick.c",
     }
 
     filter "system:windows"
-        buildoptions { "-std=c11", "-lgdi32" }
+        --buildoptions { "-std=c11", "-lgdi32" }
         systemversion "latest"
         staticruntime "On"
 
         files {
             "src/win32_init.c",
+            "src/win32_module.c",
             "src/win32_joystick.c",
             "src/win32_monitor.c",
+            "src/win32_time.h",
             "src/win32_time.c",
+            "src/win32_thread.h",
             "src/win32_thread.c",
             "src/win32_window.c",
             "src/wgl_context.c",
@@ -39,6 +54,6 @@ project "GLFW"
             "_CRT_SECURE_NO_WARNINGS"
         }
     
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+    --filter { "system:windows", "configurations:Release" }
+    --    buildoptions "/MT"
     
